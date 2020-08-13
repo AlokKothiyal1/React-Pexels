792adfad9ddcfaf1e2db1fad4b9f3cd63f8bdb55
richachauhan59 <richachuahan2019@gmail.com>
2020-08-13 13:06:25 +0530

    commit

--- pixels/src/components/Photos.jsx
+++ pixels/src/components/Photos.jsx
@@ -1,27 +1,2 @@
 import React from 'react'
 import axios from 'axios'
-
-export default class Photos extends React.Component {
-    constructor(props){
-        super(props)
-        state ={
-            photo = []
-        }
-    }
-componentDidMount(){
-    axios.get(`https://api.pexels.com/v1/search?query=nature&per_page=1&=563492ad6f917000010000017345b49ef98e449ca53af9e16c0acc5c`)
-    .then(res =>{
-        console.log(res.data)
-        const photo = res.data
-        this.setState({photo})
-    })
-}
-
-    render(){
-        return(
-            <div>
-
-            </div>
-        )
-    }
-}
